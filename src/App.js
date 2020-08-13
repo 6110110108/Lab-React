dded506563478b120ada537340ee6596e0941e4c
SEOULCHA RATMUMAD <6110110108@psu.ac.th>
2020-08-13 20:39:00 +0700

    Part 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import WordCard from './WordCard';
 
 const word = "Hello";
 function App() {
@@ -14,3 +14,17 @@ function App() {
 }
 
 export default App;
+
+/*import React from 'react';
+import './App.css';
+//import WordCard from './WordCard';
+import CharacterCard from './CharacterCard';
+//const word = "Hello";
+function App() {
+  return (
+    <div>
+      <CharacterCard/>
+    </div>
+  );
+}
+export default App;*/
