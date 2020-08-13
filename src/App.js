a7f4d62177b9dde60314ac43942718da00187de2
SEOULCHA RATMUMAD <6110110108@psu.ac.th>
2020-08-13 19:48:40 +0700

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+
+const word = "Hello";
 function App() {
   return (
-    <div className="App"> 
-        Hello world
+    <div className="App">
+        {
+          <WordCard value={word}/>
+        } 
     </div>
   );
 }
