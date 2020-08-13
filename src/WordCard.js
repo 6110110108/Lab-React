8fbbbff42aa6612e94cb5bd230fbd4220e88589b
SEOULCHA RATMUMAD <6110110108@psu.ac.th>
2020-08-13 19:52:01 +0700

    part 4

--- src/WordCard.js
+++ src/WordCard.js
@@ -5,6 +5,7 @@ export default function WordCard(props){
     return (
         <div>
             {
+                
                 Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i}/>) 
             }
         </div>
